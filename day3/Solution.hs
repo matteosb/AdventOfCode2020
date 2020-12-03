c0ed79624ff0c75335f3f145ca2db16d65731726
Matteo Banerjee <matteobanerjee@gmail.com>
2020-12-03 10:27:34 -0500

    Day 3, more point-free

--- day3/Solution.hs
+++ day3/Solution.hs
@@ -4,18 +4,18 @@
 readMap :: IO [String]
 readMap = map cycle . lines <$> readFile "./input.txt"
 
-path :: (Int, Int) -> [String] -> [Char]
-path _ [] = []
-path s@(slopeX, slopeY) m = head (head m) : path s (map (drop slopeX) (drop slopeY m))
+path :: [String] -> (Int, Int) -> [Char]
+path [] _ = []
+path m s@(slopeX, slopeY) = head (head m) : path (map (drop slopeX) (drop slopeY m)) s
 
 countTrees :: String -> Int
-countTrees m = length $ filter (== '#') m
+countTrees = length . filter (== '#')
 
 part1 :: [String] -> String
-part1 m = show . countTrees $ path (3, 1) m
+part1 m = show . countTrees $ path m (3, 1)
 
 part2 :: [String] -> String
-part2 m = show . product $ map (\s -> countTrees $ path s m) slopes
+part2 m = show . product $ map (countTrees . path m) slopes
   where
     slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
