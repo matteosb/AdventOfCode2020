4c56c82f0101f280da771eeb693a5453227cfe38
Matteo Banerjee <matteobanerjee@gmail.com>
2020-12-12 15:03:09 -0500

    Day 12, Part 2

--- day12/Solution.hs
+++ day12/Solution.hs
@@ -4,7 +4,8 @@
 import Prelude hiding (Left, Right)
 
 data Dir = North | South | East | West | Left | Right | Forward deriving (Show, Eq)
-type Action = (Dir, Int) 
+
+type Action = (Dir, Int)
 
 parseAction :: String -> Action
 parseAction str = (dir, value)
@@ -17,7 +18,7 @@ parseAction str = (dir, value)
       'L' -> Left
       'R' -> Right
       'F' -> Forward
-      _   -> error $ "Failed to parse action"  <> str 
+      _ -> error $ "Failed to parse action" <> str
     value = read $ tail str :: Int
 
 rotate :: Action -> Dir -> Dir
@@ -27,10 +28,7 @@ rotate (leftOrRight, i) currentDir = dropWhile (/= currentDir) (cycle dirs) !! d
     dirs = case leftOrRight of
       Left -> reverse directionsRight
       Right -> directionsRight
-      _     -> error "yay partial functions!" 
-  
--- rotate (Left deg) currentDir = (dropWhile (/= currentDir 0) (cycle (reverse dirsRight))) !! (floor (deg / 90))
--- rotate _ = error "rotate is only defined for Left and Right"
+      _ -> error "Action must be left or right"
 
 part1 :: [Action] -> Int
 part1 actions = let (x, y, _) = finalState in abs x + abs y
@@ -40,19 +38,34 @@ part1 actions = let (x, y, _) = finalState in abs x + abs y
     foldActions (x, y, dir) a = case a of
       (North, i) -> (x, y + i, dir)
       (South, i) -> (x, y - i, dir)
-      (East, i)  -> (x + i, y, dir)
-      (West, i)  -> (x - i, y, dir)
-      (Left, _)  -> (x, y, rotate a dir)
+      (East, i) -> (x + i, y, dir)
+      (West, i) -> (x - i, y, dir)
+      (Left, _) -> (x, y, rotate a dir)
       (Right, _) -> (x, y, rotate a dir)
       (Forward, i) -> foldActions (x, y, dir) (dir, i)
-      
-part2 :: String -> String
-part2 input = "TODO"
+
+rotateWP :: Int -> Bool -> (Int, Int) -> (Int, Int)
+rotateWP 0 _ wp = wp
+rotateWP degrees left (x, y) = rotateWP (degrees - 90) left $ if left then (- y, x) else (y, - x)
+
+part2 :: [Action] -> Int
+part2 actions = let (x, y, _) = finalState in abs x + abs y
+  where
+    finalState = foldl foldActions (0, 0, (10, 1)) actions
+    foldActions :: (Int, Int, (Int, Int)) -> Action -> (Int, Int, (Int, Int))
+    foldActions (x, y, (wx, wy)) a = case a of
+      (North, i) -> (x, y, (wx, wy + i))
+      (South, i) -> (x, y, (wx, wy - i))
+      (East, i) -> (x, y, (wx + i, wy))
+      (West, i) -> (x, y, (wx - i, wy))
+      (Left, deg) -> (x, y, rotateWP deg True (wx, wy))
+      (Right, deg) -> (x, y, rotateWP deg False (wx, wy))
+      (Forward, i) -> (x + i * wx, y + i * wy, (wx, wy))
 
 main :: IO ()
 main = do
   input <- readFile "./input.txt"
-  let actions = map parseAction $ lines input 
+  let actions = map parseAction $ lines input
   putStrLn $ "Part 1:\n" <> show (part1 actions)
   putStrLn "---------------------------------------------"
-  putStrLn $ "Part 2:\n" <> part2 input
+  putStrLn $ "Part 2:\n" <> show (part2 actions)
