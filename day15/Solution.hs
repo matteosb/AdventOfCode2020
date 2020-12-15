3f59a0d87f459af77a155a8811f426a0aaa4b50e
Matteo Banerjee <matteobanerjee@gmail.com>
2020-12-15 13:45:06 -0500

    Day 15, Part 2

--- day15/Solution.hs
+++ day15/Solution.hs
@@ -1,33 +1,29 @@
 #!/usr/bin/env stack
--- stack --resolver lts-16.23 script
+-- stack --resolver lts-16.23 script --package unordered-containers
 
-import Data.Maybe
+{-# LANGUAGE BangPatterns #-}
 
-nextNumber :: [Int] -> Int
-nextNumber spoken = fromMaybe 0 turnDiff
-  where 
-    turnDiff = previousAppearance (head spoken) 1 (tail spoken)
-    previousAppearance :: Int -> Int -> [Int] -> Maybe Int
-    previousAppearance _ _ [] = Nothing    
-    previousAppearance n c (x:xs) =
-      if n == x then Just c else previousAppearance n (c + 1) xs 
+import qualified Data.HashMap.Strict as M
 
+nextNumber :: Int -> Int -> M.HashMap Int Int -> Int
+nextNumber prev idx cache = maybe 0 (idx -) $ M.lookup prev cache
 
-genSeq :: [Int] -> Int -> [Int]
-genSeq starting total = go (reverse starting) total (length starting)
-  where go xs t l
-          | t == l = xs
-          | otherwise = go (nextNumber xs : xs) t (l + 1) 
+makeInitialCache :: [Int] -> M.HashMap Int Int
+makeInitialCache initial = M.fromList $ zip (take (length initial - 1) initial) [0 ..]
 
-part1 :: [Int] -> Int
-part1 startingNumbers = head (genSeq startingNumbers 2020)
-
-part2 :: [Int] -> Int
-part2 startingNumbers = head (genSeq startingNumbers 30000000)
+solve :: [Int] -> Int -> Int
+solve initial iters = go (last initial) (length initial) (makeInitialCache initial)
+  where
+    go !x !l !cache
+      | iters == l = x
+      | otherwise =
+        let idx = l - 1
+            next = nextNumber x idx cache
+         in go next (l + 1) (M.insert x idx cache)
 
 main :: IO ()
 main = do
-  let startingNumbers = [19, 20, 14, 0, 9, 1]
-  putStrLn $ "Part 1:\n" <> show (part1 startingNumbers)
+  let initialNumbers = [19, 20, 14, 0, 9, 1]
+  putStrLn $ "Part 1:\n" <> show (solve initialNumbers 2020)
   putStrLn "---------------------------------------------"
-  putStrLn $ "Part 2:\n" <> show (part2 startingNumbers)
+  putStrLn $ "Part 2:\n" <> show (solve initialNumbers 30000000)
